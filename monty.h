8bf15dc47520bb13efa126d7023774e15e6335ac
eddyamon <eddyamon@gmail.com>
2023-12-14 15:01:03 -0800

    monty

---
+++ monty.h
@@ -0,0 +1,26 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <funt1.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+
+
+
+void e_push(stack_t **head, unsigned int number);
+void e_pall(stack_t **head, unsigned int number);
+
+
+
+
+
+
+
+
+#endif
