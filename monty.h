e1bc9c9c3760851a81851726dba6e2ec279b6715
eddyamon <eddyamon@gmail.com>
2023-12-14 15:08:24 -0800

    monty

--- monty.h
+++ monty.h
@@ -7,12 +7,50 @@
 #include <funt1.h>
 #include <string.h>
 #include <ctype.h>
-
-
-
-
-
-
+/**
+ * struct stacks_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stacks_s
+{
+	int n;
+	struct stacks_s *prev;
+	struct stacks_s *next;
+} stack_t;
+/**
+ * struct bus_ss - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_ss
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
+/**
+ * struct instructions_ - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instructions_
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 void e_push(stack_t **head, unsigned int number);
 void e_pall(stack_t **head, unsigned int number);
 
